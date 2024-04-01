dd37eb22ce373d7784d28cd4e64a8acf111f9d91
BenChun <67034610+BenjaminChun@users.noreply.github.com>
2024-04-01 15:53:18 +0800

    fix: changing read req to pass entire file

--- tcp_server/server/src/ReadRequest.cpp
+++ tcp_server/server/src/ReadRequest.cpp
@@ -17,24 +17,27 @@ ReadRequest::ReadRequest(int uniqueID, int opcode, const std::string& pathName,
         std::cout << "numBytesToRead: " << numBytesToRead << std::endl;
       }
 
-// returns buffer, if empty failure in reading file
-std::vector<char> readFile2(const std::string& filePath, int offset,
-                           int numBytesToRead) {
-    // initialise buffer
-    std::vector<char> buffer(numBytesToRead);
+std::vector<char> readFile2(const std::string& filePath) {
+    // Initialise buffer
+    std::vector<char> buffer;
 
-    // opens file in binary stream
+    // Open file in binary stream
     std::ifstream file(filePath, std::ios::binary);
     if (!file.is_open()) {
         std::cerr << "Error: Unable to open file " << filePath << std::endl;
         return buffer;  // Return an empty buffer indicating failure
     }
 
-    // Seek to the specified offset
-    file.seekg(offset);
+    // Get the size of the file
+    file.seekg(0, std::ios::end);
+    std::streampos fileSize = file.tellg();
+    file.seekg(0, std::ios::beg);
 
-    // Read the specified number of bytes into the buffer
-    file.read(buffer.data(), numBytesToRead);
+    // Resize buffer to fit the file contents
+    buffer.resize(fileSize);
+
+    // Read file contents into the buffer
+    file.read(buffer.data(), fileSize);
 
     // Close the file
     file.close();
@@ -53,8 +56,7 @@ Response ReadRequest::process() {
     // initialise buffer
     std::vector<char> buffer(ReadRequest::numBytesToRead);
     // read file path
-    buffer = readFile2(ReadRequest::pathName, ReadRequest::offset,
-                      ReadRequest::numBytesToRead);
+    buffer = readFile2(ReadRequest::pathName);
 
     std::cout << "Processed ReadRequest for " << pathName << " with offset "
               << offset << " and numBytesToRead " << numBytesToRead
